6d6c87a8d6556501c04f1a016acbdf1aac8e4f2d
Aleksandr0012 <32939018+Aleksandr0012@users.noreply.github.com>
2020-06-15 00:13:33 +0400

    Updates

---
+++ precache-manifest.9d8d84dab8b0820f8e74a0c3c919a513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c20ca39529d61b23c7a3c4340abf93",
+    "url": "/copypast/index.html"
+  },
+  {
+    "revision": "2aedc5b3fc6d068ece1a",
+    "url": "/copypast/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c69b01287a84a70e2870",
+    "url": "/copypast/static/js/2.5f1632b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/copypast/static/js/2.5f1632b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aedc5b3fc6d068ece1a",
+    "url": "/copypast/static/js/main.08a0b1b3.chunk.js"
+  },
+  {
+    "revision": "c57d155a6bf1f68c5adf",
+    "url": "/copypast/static/js/runtime-main.9d6d8a9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/copypast/static/media/logo.5d5d9eef.svg"
+  }
+]);
