470c6872988ddade5344852eb9ec60351ce0a0f5
Aleksandr0012 <32939018+Aleksandr0012@users.noreply.github.com>
2020-06-26 01:12:52 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import { Grid } from 'semantic-ui-react';
+
 
 function App() {
   return (
@@ -8,18 +11,37 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Copy Past
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <iframe title="title" src="https://jsfiddle.net/aapodshivalov/c1js68f9/show"></iframe>
       </header>
+
+        <Grid columns={2} divided='vertically' celled='internally'>
+          <Grid.Row>
+            <Grid.Column>
+                LIST
+            </Grid.Column>
+            <Grid.Column>
+              <iframe title="title" src="https://jsfiddle.net/aapodshivalov/c1js68f9/show"></iframe>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+                COLORS
+            </Grid.Column>
+            <Grid.Column>
+              <iframe title="title2" src="https://jsfiddle.net/aapodshivalov/Ljkh1xcv/show"></iframe>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+                PARTY
+            </Grid.Column>
+            <Grid.Column>
+              <iframe title="title3" src="https://jsfiddle.net/aapodshivalov/fzgym5jn/show"></iframe>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
     </div>
   );
 }
